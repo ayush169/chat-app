b2f1673da7f403b48d25538c26ef6c25443c6c1f
ayush dubey <ayushdubeydps@gmail.com>
2021-09-11 21:17:21 +0530

    added firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyA8CixEalg4ZgLRQK9QAA92e1n8shlHNfc",
+  authDomain: "chat-web-app-768fb.firebaseapp.com",
+  projectId: "chat-web-app-768fb",
+  storageBucket: "chat-web-app-768fb.appspot.com",
+  messagingSenderId: "122227243529",
+  appId: "1:122227243529:web:fdc68e5aca68fcfea3d6d8",
+};
+
+const app = firebase.initializeApp(config);
