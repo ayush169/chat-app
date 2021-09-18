5f90dea1ec4cdc9a18a7fd59b9b1ebdb5e93a6da
ayush dubey <ayushdubeydps@gmail.com>
2021-09-18 09:33:03 +0530

    added hover

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -6,9 +6,12 @@ import ProfileAvatar from "../../dashboard/ProfileAvatar";
 import PresenceDot from "../../PresenceDot";
 import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
 import { auth } from "../../../misc/firebase";
+import { useHover } from "../../../misc/custom-hooks";
 
 const MessageItem = ({ message, handleAdmin }) => {
   const { author, createdAt, text } = message;
+
+  const [selfRef, isHovered] = useHover();
 
   const isAdmin = useCurrentRoom((v) => v.isAdmin);
   const admins = useCurrentRoom((v) => v.admins);
@@ -19,7 +22,10 @@ const MessageItem = ({ message, handleAdmin }) => {
   const canGrantAdmin = isAdmin && !isAuthor;
 
   return (
-    <li className="padded mb-1">
+    <li
+      className={`padded mb-1 cursor-pointer ${isHovered ? "bg-black-02" : ""}`}
+      ref={selfRef}
+    >
       <div className="d-flex align-items-center font-bolder mb-1"></div>
       <PresenceDot uid={author.uid} />
 
