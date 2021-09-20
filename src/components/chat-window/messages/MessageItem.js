17795c78f8d8f96619f326f50727286b0a954ba3
ayush dubey <ayushdubeydps@gmail.com>
2021-09-20 12:56:34 +0530

    handle likes

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -6,12 +6,14 @@ import ProfileAvatar from "../../dashboard/ProfileAvatar";
 import PresenceDot from "../../PresenceDot";
 import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
 import { auth } from "../../../misc/firebase";
-import { useHover } from "../../../misc/custom-hooks";
+import { useHover, useMediaQuery } from "../../../misc/custom-hooks";
+import IconBtnControl from "./IconBtnControl";
 
-const MessageItem = ({ message, handleAdmin }) => {
-  const { author, createdAt, text } = message;
+const MessageItem = ({ message, handleAdmin, handleLike }) => {
+  const { author, createdAt, text, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
+  const isMobile = useMediaQuery("(max-width: 992px)");
 
   const isAdmin = useCurrentRoom((v) => v.isAdmin);
   const admins = useCurrentRoom((v) => v.admins);
@@ -21,38 +23,51 @@ const MessageItem = ({ message, handleAdmin }) => {
 
   const canGrantAdmin = isAdmin && !isAuthor;
 
+  const canShowIcons = isMobile || isHovered;
+  const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
+
   return (
     <li
       className={`padded mb-1 cursor-pointer ${isHovered ? "bg-black-02" : ""}`}
       ref={selfRef}
     >
-      <div className="d-flex align-items-center font-bolder mb-1"></div>
-      <PresenceDot uid={author.uid} />
+      <div className="d-flex align-items-center font-bolder mb-1">
+        <PresenceDot uid={author.uid} />
 
-      <ProfileAvatar
-        src={author.avatar}
-        name={author.name}
-        className="ml-1"
-        size="xs"
-      />
+        <ProfileAvatar
+          src={author.avatar}
+          name={author.name}
+          className="ml-1"
+          size="xs"
+        />
 
-      <ProfileInfoBtnModal
-        profile={author}
-        appearance="link"
-        className="p-0 ml-1 text-black"
-      >
-        {canGrantAdmin && (
-          <Button block onClick={() => handleAdmin(author.uid)} color="blue">
-            {isMsgAuthorAdmin
-              ? "Remove admin permission"
-              : "Give admin in the room"}
-          </Button>
-        )}
-      </ProfileInfoBtnModal>
-      <TimeAgo
-        datetime={createdAt}
-        className="font-normal text-black-45 ml-2"
-      />
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        >
+          {canGrantAdmin && (
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+              {isMsgAuthorAdmin
+                ? "Remove admin permission"
+                : "Give admin in the room"}
+            </Button>
+          )}
+        </ProfileInfoBtnModal>
+        <TimeAgo
+          datetime={createdAt}
+          className="font-normal text-black-45 ml-2"
+        />
+        <IconBtnControl
+          {...(isLiked ? { color: "red" } : {})}
+          isVisible={canShowIcons}
+          iconName="heart"
+          tooltip="Like this message"
+          onClick={() => handleLike(message.id)}
+          badgeContent={likeCount}
+        />
+      </div>
+
       <div>
         <span className="word-break-all">{text}</span>
       </div>
